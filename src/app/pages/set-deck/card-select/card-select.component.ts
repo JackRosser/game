55e7282d9c50a87df04463ba3a36c2b4580b27aa
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-22 20:26:40 +0200

    box select

---
+++ src/app/pages/set-deck/card-select/card-select.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-select',
+  templateUrl: './card-select.component.html',
+  styleUrl: './card-select.component.scss'
+})
+export class CardSelectComponent {
+
+}
--- src/app/pages/set-deck/set-deck.module.ts
+++ src/app/pages/set-deck/set-deck.module.ts
@@ -4,12 +4,18 @@ import { SetDeckRoutingModule } from './set-deck-routing.module';
 import { SetDeckComponent } from './set-deck.component';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { SharedModule } from '../../shared/shared.module';
+import { ViewDetailsComponent } from './view-details/view-details.component';
+import { DeckCentralComponent } from './deck-central/deck-central.component';
+import { CardSelectComponent } from './card-select/card-select.component';
 
 
 
 @NgModule({
   declarations: [
     SetDeckComponent,
+    ViewDetailsComponent,
+    DeckCentralComponent,
+    CardSelectComponent,
   ],
   imports: [
     CommonModule,
