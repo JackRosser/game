b0cfd667d3aeab97ab54de9c40ce083c5c990fb5
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-25 22:38:20 +0200

    grafica player

--- src/app/pages/arena/player/player.component.ts
+++ src/app/pages/arena/player/player.component.ts
@@ -15,6 +15,20 @@ playerList!:iMonster[]
 
 playerInGame!:iMonster
 
+// REGOLAZIONE STILE GRAFICO
+
+hpBar:number = 100
+
+full:string = "p-2 border bg-green-500 transition-all duration-500"
+danger:string = "p-2 border bg-orange-400 transition-all duration-500"
+death:string = "p-2 border bg-red-600 transition-all duration-500"
+
+cardClass:string = this.full
+cardStyle:string = `width: ${this.full}%`
+
+scale:number = 1
+
+// REGOLAZIONE STILE GRAFICO
 
 ngOnInit() {
   this.deckSvc.deck$.subscribe(list => {
